7956ae2930bb9c47fd34b2ead960bdd4e5cb0a2d
tpei <t.s.peikert@gmail.com>
2017-01-29 09:27:30 +0100

    remove fail whitespace

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ token = ENV['BOT_TOKEN']
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     if message.text
-      response_message  = CommandWatcher.handle(message)
+      response_message = CommandWatcher.handle(message)
       bot.api.send_message(chat_id: message.chat.id, text: response_message)
     end
   end
