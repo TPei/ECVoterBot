75d6335062055b8892cdda0d5216ff75efd16dd0
tpei <t.s.peikert@gmail.com>
2017-01-29 14:11:38 +0100

    restart bot on api error (does this even work?)

--- main.rb
+++ main.rb
@@ -5,15 +5,21 @@ require './lib/command_watcher'
 
 token = ENV['BOT_TOKEN']
 
-Telegram::Bot::Client.run(token) do |bot|
-  bot.listen do |message|
-    if message.text
-      begin
-        response_message = CommandWatcher.handle(message)
-        bot.api.send_message(chat_id: message.chat.id, text: response_message)
-      rescue => e
-        puts "failed #{e}"
+while(true)
+  begin
+    Telegram::Bot::Client.run(token) do |bot|
+      bot.listen do |message|
+        if message.text
+          begin
+            response_message = CommandWatcher.handle(message)
+            bot.api.send_message(chat_id: message.chat.id, text: response_message)
+          rescue => e
+            puts "failed #{e}"
+          end
+        end
       end
     end
+  rescue => e
+    puts "global error: #{e}"
   end
 end
